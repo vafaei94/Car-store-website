d341a030ba166affdb1b90a90961f0b6b48c0f48
Mohammad Reza <mr.vafaei94@gmail.com>
2025-05-29 16:58:10 +0330

    Shop - js for State

--- Shop/script.js
+++ Shop/script.js
@@ -1,9 +1,12 @@
-document.getElementById("btn-estimate").addEventListener("click", calTotoal);
-
-function calTotoal() {
 var state = document.getElementById("s-state");
-  if(state.value ===''){
-    alert("Please choose your state");
-    state.focus;
+var btn = document.getElementById("btn-estimate");
+document.addEventListener("DOMContentLoaded", function () {
+  btn.disabled = true;
+});
+state.addEventListener("change", function () {
+  if (state.value === "") {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
   }
-}
+});
